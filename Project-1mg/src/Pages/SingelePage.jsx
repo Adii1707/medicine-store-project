af91fe92b274729ac383a98875baafb3308ea279
prashantshetti07 <104505834+prashantshetti07@users.noreply.github.com>
2022-12-15 17:19:34 +0530

    update

---
+++ Project-1mg/src/Pages/SingelePage.jsx
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom"
+
+const SinglePage=()=>{
+   const {id}=useParams()
+  // console.log(id)
+    return (
+        <div>
+       
+        </div>
+    )
+}
+
+export {SinglePage}

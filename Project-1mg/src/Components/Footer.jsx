66845179fd41cddc8756c8607a9360cb84e6992b
email <shekharjoshiofficial@gmail.com>
2022-12-18 21:49:51 +0530

    footer testing

--- Project-1mg/src/Components/Footer.jsx
+++ Project-1mg/src/Components/Footer.jsx
@@ -14,41 +14,43 @@ const Footer = () => {
   return (
     <>
       <hr />
-      <Box width="80%" margin="auto" height="150px" paddingBottom="0px">
-        <Heading as="h2" mt="1.5rem" mb="1rem" fontSize="2xl" fontWeight="450">
+      <Box width={["80%","80%","80%"]} margin="auto" height="150px" paddingBottom="0px">
+        <Heading as="h2" mt="1.5rem" mb="1rem" fontSize={{ base: '15px', md: '22px', lg: '28px' }} fontWeight="450">
           INDIA’S LARGEST HEALTHCARE PLATFORM
         </Heading>
 
-        <Box>
-          <Grid templateColumns="repeat(3, 1fr)" gap={2}>
+        <Box w={["100%","100%","100%"]} >
+          <Grid templateColumns={"repeat(3, 1fr)"} gap={{base:7,md:4, lg:3}}   >
             <Box>
               <Heading as="h1">260m+</Heading>
-              <Text fontSize="md"> Visitors</Text>
+              <Text fontSize={{base:"12px", md:"15px"}}> Visitors</Text>
             </Box>
             <Box>
               <Heading as="h1">31m+</Heading>
-              <Text fontSize="md"> Orders Delivered</Text>
+              <Text fontSize={{base:"12px", md:"15px"}}> Orders Delivered</Text>
             </Box>
             <Box>
               <Heading as="h1">1800+</Heading>
-              <Text fontSize="md">Cities </Text>
+              <Text fontSize={{base:"12px", md:"15px"}}>Cities </Text>
             </Box>
           </Grid>
         </Box>
       </Box>
       <hr style={{ width: "85%", margin: "auto" }} />
       <Box
-        width="60%"
+        width="90%"
         margin="auto"
         paddingRight="40px"
+        
         paddingLeft="40px"
         height="90px"
         display="flex"
+       
         justifyContent="center"
         alignItems="center"
       >
-        <Grid templateColumns="repeat(2, 1fr)" gap={0} alignItems="center">
-          <Heading fontSize="19.1px" fontWeight="450" as="h3">
+        <Grid templateColumns={{ base:"repeat(1, 1fr)", md:"repeat(2,1fr)"}} gap={{base:1,md:0 ,lg:0}} alignItems="center" >
+          <Heading    width={{base:"90%", md:"80%",lg:"100%"}} fontSize={{base:"18px",md:"19px",lg:"20.1px"}} fontWeight="450" as="h3">
             Get the link to download App
           </Heading>
           <Box
@@ -56,6 +58,7 @@ const Footer = () => {
             margin="auto"
             marginRight="10px"
             display="flex"
+           
             justifyContent="center"
             alignItems="center"
             gap="10px"
@@ -70,8 +73,8 @@ const Footer = () => {
             <Button
               color="white"
               backgroundColor="#ff6f61"
-              width="7rem"
-              fontSize="12px"
+              width={["3rem","5rem","7rem"]}
+              fontSize={{base:"8px",md:"10px",lg:"12px"}}
               _hover={{ bg: "green.300" }}
             >
               Send Link
@@ -86,112 +89,114 @@ const Footer = () => {
         bgColor="#fffcf8"
         width="100%"
         height="300px"
+        border="1px solid red"
       >
         <Box
           marginTop="20px"
           marginLeft="25px"
           width="70%"
           height="250px"
+          border="1px solid black"
           pl={10}
           pr={20}
         >
-          <Grid templateColumns="repeat(4, 1fr)" gap={10}>
+          <Grid templateColumns={{ base:"repeat(1, 1fr)", md:"repeat(1,1fr)",lg:"repeat(4,1fr)"}} gap={10}>
             <VStack cursor="pointer" align={"flex-start"} spacing={1}>
-              <Heading mb="1rem" fontSize="16px" fontWeight="400" as="h3">
+              <Heading mb="1rem" fontSize={{base:"25px",md:"20px", lg:"16px"}} fontWeight={{base:"600",md:"500", lg:"400"}}as="h3">
                 Know Us
               </Heading>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
                 About Us
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Contact Us{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Contact Us
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Press Coverage{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Press Coverage
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Careers{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Careers
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Business Partnership{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Business Partnership
               </Text>
 
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Become a Health Partner{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Become a Health Partner
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
                 Corporate Governance
               </Text>
             </VStack>
 
             <VStack cursor="pointer" align={"flex-start"} spacing={1}>
-              <Heading mb="1rem" fontSize="16px" fontWeight="400" as="h3">
+              <Heading mb="1rem"fontSize={{base:"25px",md:"20px", lg:"16px"}} fontWeight={{base:"600",md:"500", lg:"400"}} as="h3">
                 Our Policies
               </Heading>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
                 Privacy Policy
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
                 Term and Conditions
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Editorial Policy{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Editorial Policy
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Return Policy{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Return Policy
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                IP Policy{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                IP Policy
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                Grievance Redressal Policy{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                Grievance Redressal Policy
               </Text>
 
-              <Text color="gray.600" fontSize="12px">
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Fake Jobs and Fraud Disclaimer
               </Text>
             </VStack>
 
             <VStack cursor="pointer" align={"flex-start"} spacing={1}>
-              <Heading mb="1rem" fontSize="16px" fontWeight="400" as="h3">
+              <Heading mb="1rem" fontSize={{base:"25px",md:"20px", lg:"16px"}} fontWeight={{base:"600",md:"500", lg:"400"}} as="h3">
                 Our Services
               </Heading>
-              <Text color="gray.600" fontSize="12px">
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Order Medicines
               </Text>
-              <Text color="gray.600" fontSize="12px">
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Book Lab Tests
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                {" "}
-                Consult a Doctor{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
+                Consult a Doctor
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                Ayurveda Articles{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                Ayurveda Articles
               </Text>
-              <Text color="gray.600" fontSize="12px">
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Hindi Articles
               </Text>
-              <Text color="gray.600" fontSize="12px">
-                Care Plan{" "}
+              <Text color="gray.600" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                Care Plan
               </Text>
             </VStack>
 
             <VStack cursor="pointer">
-              <Heading mb="1rem" fontSize="16px" fontWeight="400" as="h3">
-                Connect{" "}
+              <Heading mb="1rem" fontSize={{base:"25px",md:"20px", lg:"16px"}} fontWeight={{base:"600",md:"500", lg:"400"}} as="h3">
+                Connect
               </Heading>
               <Text color="gray.600" fontSize="13px" fontWeight="semibold">
                 Social Links
@@ -232,7 +237,7 @@ const Footer = () => {
               </HStack>
 
               <Text color="gray.500" fontSize="15px">
-                Want daily dose of health?{" "}
+                Want daily dose of health?
               </Text>
               <Button
                 color="white"
@@ -269,28 +274,29 @@ const Footer = () => {
         </Box>
       </Box>
       <hr style={{ width: "85%", margin: "auto" }} />
-      <Box margin="auto" textAlign="start" w="80%" h="200px" bgColor="#fffcf8">
-        <Grid templateColumns="repeat(3, 1fr)" gap={2}>
+      <Box  margin={{base:"none", md:"none",lg:"auto"}} textAlign="start" w={["50%","60%","80%"]} h="auto"  bgColor="#fffcf8">
+        <Box w="100%" h="auto" border="1px solid black" display="flex" flexDirection={{base:"column", md:"column", lg:"row", xl:"row"}} gap={2}>
           <Box
             display="flex"
+            
             gap="20px"
             justifyContent="center"
             alignItems="center"
           >
             <Box>
-              {" "}
-              <Image src="https://onemg.gumlet.io/secure-rebrand_x6f8yq.svg" />{" "}
+              
+              <Image src="https://onemg.gumlet.io/secure-rebrand_x6f8yq.svg" />
             </Box>
-            <Box w="200px">
-              <Heading mt={5} mb={2} fontSize="30px" as="h3">
-                {" "}
-                Reliable{" "}
+            <Box  w="200px">
+              <Heading mt={5} mb={2} fontSize={{base:"20px",md:"25px", lg:"30px"}} as="h3">
+                
+                Reliable
               </Heading>
-              <Text color="gray.400" fontSize="12px">
-                {" "}
+              <Text color="gray.400" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
+                
                 All products displayed on Tata 1mg are procured from verified
                 and licensed pharmacies. All labs listed on the platform are
-                accredited{" "}
+                accredited
               </Text>
             </Box>
           </Box>
@@ -304,11 +310,11 @@ const Footer = () => {
               <Image src="https://onemg.gumlet.io/reliable-rebrand_rcpof3.svg" />
             </Box>
             <Box w="200px">
-              <Heading mt={5} mb={2} fontSize="30px" as="h3">
-                {" "}
-                Secure{" "}
+              <Heading mt={5} mb={2} fontSize={{base:"20px",md:"25px", lg:"30px"}} as="h3">
+                
+                Secure
               </Heading>
-              <Text color="gray.400" fontSize="12px">
+              <Text color="gray.400" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Tata 1mg uses Secure Sockets Layer (SSL) 128-bit encryption and
                 is Payment Card Industry Data Security Standard (PCI DSS)
                 compliant
@@ -325,18 +331,18 @@ const Footer = () => {
               <Image src="https://onemg.gumlet.io/affordable-rebrand_ivgidq.svg" />
             </Box>
             <Box w="200px">
-              <Heading mt={5} mb={2} fontSize="30px" as="h3">
-                {" "}
-                Affordable{" "}
+              <Heading mt={5} mb={2} fontSize={{base:"20px",md:"25px", lg:"30px"}} as="h3">
+                
+                Affordable
               </Heading>
-              <Text color="gray.400" fontSize="12px">
+              <Text color="gray.400" fontSize={{base:"20px",md:"18px", lg:"12px"}}>
                 Find affordable medicine substitutes, save up to 50% on health
                 products, up to 80% off on lab tests and free doctor
                 consultations.
               </Text>
             </Box>
           </Box>
-        </Grid>
+        </Box>
       </Box>
       <hr style={{ width: "85%", margin: "auto" }} />
       <Box
